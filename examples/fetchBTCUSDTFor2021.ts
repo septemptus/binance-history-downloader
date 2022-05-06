2803a41b96c59a01c7a699f7b507d03474fa389d
Septemptus <sephiran@gmail.com>
2022-05-06 23:41:50 +0200

    Modify example to indicate that the data is parsed

--- examples/fetchBTCUSDTFor2021.ts
+++ examples/fetchBTCUSDTFor2021.ts
@@ -1,9 +1,17 @@
 import { getBinanceData } from '../src/fetcher';
-import { KlineData } from '../src/parser';
 
 /*
   This will fetch parsed kline data for BTCUSDT in 24 hours klines starting from January 2021 ending in December 2021
 */
-export default async function fetchBTCUSDTFor2021(): Promise<KlineData[]> {
-  return await getBinanceData('BTCUSDT', '1d', new Date('2021-01-01'), new Date('2021-12-01'));
+export default async function fetchBTCUSDTFor2021(): Promise<void> {
+  const data = await getBinanceData(
+    'BTCUSDT',
+    '1d',
+    new Date('2021-01-01'),
+    new Date('2021-12-01')
+  );
+
+  data.forEach((kline) => {
+    console.log(kline.open, kline.close);
+  });
 }
